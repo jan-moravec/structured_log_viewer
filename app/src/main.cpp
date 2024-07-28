8dad0f0b5505829507ef17765249c36935a3e05d
Moravec, Jan <jan.moravec@hidglobal.com>
2024-07-28 12:25:11 +0200

    Set style

--- app/src/main.cpp
+++ app/src/main.cpp
@@ -1,10 +1,12 @@
 #include "main_window.hpp"
 
 #include <QApplication>
+#include <QStyleFactory>
 
 int main(int argc, char *argv[])
 {
     QApplication a(argc, argv);
+    a.setStyle(QStyleFactory::create("windows11")); // "windows11", "windowsvista", "Windows", "Fusion"
     MainWindow w;
     w.show();
     return a.exec();
