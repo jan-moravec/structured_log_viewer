8c31a3eb38130ed684165677788f5ce259081fa9
Jan Moravec <jan.moravec@hidglobal.com>
2025-04-28 09:39:30 +0200

    Add very basic Qt app test

---
+++ test/app/src/main_window_test.cpp
@@ -0,0 +1,53 @@
+#include "log_filter_model.hpp"
+#include "log_model.hpp"
+#include "log_table_view.hpp"
+#include "main_window.hpp"
+#include <QtTest/QtTest>
+
+class MainWindowTest : public QObject
+{
+    Q_OBJECT
+
+private slots:
+    void initTestCase()
+    {
+        // Called before the first test function
+        qDebug() << "Starting MainWindow tests";
+    }
+
+    void cleanupTestCase()
+    {
+        // Called after the last test function
+        qDebug() << "MainWindow tests complete";
+    }
+
+    void init()
+    {
+        // Called before each test function
+        window = new MainWindow();
+    }
+
+    void cleanup()
+    {
+        // Called after each test function
+        delete window;
+        window = nullptr;
+    }
+
+    // Test methods
+    void testWindowTitle()
+    {
+        QCOMPARE(window->windowTitle(), QString("Structured Log Viewer"));
+    }
+
+    void testWindowIcon()
+    {
+        QVERIFY(!window->windowIcon().isNull());
+    }
+
+private:
+    MainWindow *window;
+};
+
+QTEST_MAIN(MainWindowTest)
+#include "main_window_test.moc"
